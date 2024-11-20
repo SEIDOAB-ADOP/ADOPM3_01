c84b4550ea91c8d940f0e8a0c1d0f0c3e005ea18
Martin <martin_lenart@icloud.com>
2024-11-20 18:13:30 +0900

    initial commit

---
+++ ADOPM3_01_02a/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ADOPM3_01_02a
+{
+    class Program
+    {
+        public struct AStruct { public int iVal; }
+        static void Main(string[] args)
+        {
+            //Boxed value type (ox1 and ox2) becomes reference types, BUT does NOT behave as normal
+            //reference types as
+            //  - a copies of the value type is boxed into the heap and unboxed into the stack
+            //  - modifications of the instance on the heap i not allowed. Compiler error
+            //Therefore, a boxed type still behaves as a value type
+
+            //Example: AStruct is a complex, classic value type
+            AStruct s1 = new AStruct { iVal = 9 };
+            AStruct s2 = new AStruct { iVal = 7 };
+
+            s1 = s2;    // deep copy, i.e. the content
+            s2.iVal = 10;
+            Console.WriteLine($"s1: {s1.iVal}, s2: {s2.iVal}"); //7,10 expected value type behaviour
+
+            
+            //Boxing the struct - it is now of reference type object
+            object bs1 = s1;
+            object bs2 = s2;  // shallow copy, i.e. the reference only
+
+            bs1 = bs2;  //ox1 and ox2 refer to the same instance on the heap
+            Console.WriteLine($"bs1: {((AStruct)(bs1)).iVal}, bs2: {((AStruct)(bs2)).iVal}");
+
+            
+            //((AStruct)(bs2)).iVal = 20;     //Compiler error when trying to change the boxed object on the heap
+            bs2 = new AStruct { iVal = 20 };  //bs2 now refers to a new boxed instance on the heap
+            Console.WriteLine($"bs1: {((AStruct)(bs1)).iVal}, bs2: {((AStruct)(bs2)).iVal}"); // 10, 20 expected value type behaviour
+
+            //Unbox the bi1, bi2 (variables of reference type) copies back into value types
+            //object must be casted to the boxed type
+            AStruct ubs1 = (AStruct)bs1;
+            AStruct ubs2 = (AStruct)bs2;
+            Console.WriteLine($"ubs1: {((AStruct)(ubs1)).iVal}, ubs2: {((AStruct)(ubs2)).iVal}"); // 10, 20
+            
+        }
+    }
+}
---
+++ ADOPM3_01_07/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace ADOPM3_01_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string s = null;
+
+			//Using statement with some Exception handling
+			using (WebClient wc = new WebClient())
+			{
+				try { s = wc.DownloadString("http://www.microsoft.com/"); }
+				catch (WebException ex)
+				{
+					if (ex.Status == WebExceptionStatus.NameResolutionFailure)
+						Console.WriteLine("Bad domain name");
+					else
+						throw;     // Can’t handle other sorts of WebException, so rethrow
+				}
+			}
+
+			//s now contains the html markup from the website
+			File.WriteAllText(fname("Example4_07.txt"), s);
+			if (File.Exists(fname("Example4_07.txt")))
+			{
+				//readback using StreamReader in a using declaration
+				using var reader = File.OpenText(fname("Example4_07.txt")); //Using declaration
+				Console.WriteLine(reader.ReadToEnd());
+
+			}
+
+			//using var reader = ... is now out of scope and the using declaration has closed the stream
+
+
+			//Local helper method to ge platform independant paths
+			static string fname(string name)
+			{
+				//LocalApplicationData is a good place to store a temporary file
+				var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+				documentPath = Path.Combine(documentPath, "AOOP2", "Examples");
+				if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
+				return Path.Combine(documentPath, name);
+			}
+		}
+	}
+
+	//Exercise:
+	//1.	Change the domain name in order to generate different Web Exceptions (www.microsoft1.com and www.m.com. 
+	//		Explain what happens
+	//2.	Try to delete the file File.Delete() in the if statement. Explain what happens and why
+}
